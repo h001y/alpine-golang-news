4a4e8ff47373630ba358d74dd549f163de51815b
hoo1y <hoo1y@yandex.ru>
2021-10-11 21:41:52 +0300

    add Article struct

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
@@ -51,4 +52,15 @@ func main() {
 type Source struct {
 	ID   interface{} `json:"id"`
 	Name string      `json:"name"`
+}
+
+type Article struct {
+	Source      Source    `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
 }
